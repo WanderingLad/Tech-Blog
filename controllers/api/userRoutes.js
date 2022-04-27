ed5f65c8c9c6950f118687c0ff7f3ea567129380
James Reilly <jreilly2112@gmail.com>
2022-04-27 18:42:57 -0400

    user creation routes created

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,7 +5,9 @@ const { readAndAppend, readFromFile } = require('../../utils/fsUtils');
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
-    
+
+    readAndAppend(userData, './seeds/userData.json');
+
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
@@ -40,7 +42,7 @@ router.post('/login', async (req, res) => {
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-      
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
 
